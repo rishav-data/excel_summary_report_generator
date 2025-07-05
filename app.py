64e018efa469dbb9331f30b228699ff193728625
Rishav Dhara <mr.oopsthefirst17@gmail.com>
2025-07-05 16:12:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ def generate_report():
     return send_file(report_path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=10000)
